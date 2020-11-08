432c8965b2e39ad271fce18b357c38c4578b49f6
ava <ava239@yandex.ru>
2020-11-09 02:31:28 +0300

    Add solution and more tests

--- src/ResaleCalculator.php
+++ src/ResaleCalculator.php
@@ -6,7 +6,18 @@ class ResaleCalculator
 {
     public function calculateLowestLoss(array $prices): int
     {
-        $lowest = 0;
+        $lowest = max($prices);
+        foreach ($prices as $buyDay => $buyPrice) {
+            for ($i = $buyDay + 1; $i < count($prices); $i += 1) {
+                $diff = $buyPrice - $prices[$i];
+                if ($buyPrice > $prices[$i] && $diff < $lowest) {
+                    $lowest = $diff;
+                }
+                if ($buyPrice < $lowest) {
+                    $lowest = $buyPrice;
+                }
+            }
+        }
 
         return $lowest;
     }
--- tests/LossTest.php
+++ tests/LossTest.php
@@ -19,9 +19,20 @@ class LossTest extends TestCase
 
     public function dataProvider()
     {
+        $long = [];
+        for ($i = 0; $i < 1000; $i += 1) {
+            $long[] = $i + 10;
+        }
+        $shuffled = $long;
+        shuffle($shuffled);
         return [
             [[210, 130, 50, 175, 100], 30],
-            [[210, 130, 50, 175, 80], 45],
+            [[210, 130, 50, 175, 80], 35],
+            [[1000], 1000],
+            [[10, 20, 30, 40, 50, 60, 70, 80], 10],
+            [[30, 40, 50, 70, 70, 60, 20], 10],
+            [$long, 10],
+            [$shuffled, 1]
         ];
     }
 }
